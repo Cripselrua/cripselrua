165ee17232fdd09120736a03b31ba7a2497ca403
Cripselrua <90082950+Cripselrua@users.noreply.github.com>
2021-11-04 00:02:56 +0200

    Add files via upload

---
+++ js/burger.js
@@ -0,0 +1,18 @@
+
+$(document).ready(function() {
+    $('.menu_icon').click(function(event){
+        $('.menu_icon,.menu_body').toggleClass('active');
+      $('body').toggleClass('lock');         // блокирует прокрутку при аткрывание бургера body.lock{overflow: hidden;}
+        
+    
+    });
+});
+
+
+$(document).ready(function() {
+    $('.menu_link, #menu_link').click(function(event) {
+        $('.menu_icon, .menu_body').removeClass('active');
+        $('body').removeClass('lock');
+    });
+});
+
