5d9365d21d00cf37d2c27a8d4e1b48e005e684ed
Cripselrua <90082950+Cripselrua@users.noreply.github.com>
2021-11-04 21:09:02 +0200

    Add files via upload

---
+++ js/scrol.js
@@ -0,0 +1,41 @@
+const menuLinks = document.querySelectorAll('.menu_link[data-goto]');
+if (menuLinks.length > 0) {
+    menuLinks.forEach(menuLink => {
+        menuLink.addEventListener("click", onMenuLinkClick);
+    });
+
+    function onMenuLinkClick(e) {
+        const menuLink = e.target;
+        if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+            const gotoBlock = document.querySelector(menuLink.dataset.goto);
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('.header_container').offsetHeight;
+
+            window.scrollTo({
+                top: gotoBlockValue,
+                behavior: "smooth"
+            });
+            e.preventDefault();
+        }
+    }
+}
+// =========================================================================================================================================
+
+const anchors = document.querySelectorAll('a#menu_link')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href')
+    
+    document.querySelector(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
+
+$(function(){
+  $("#DivContent").load("popup.html"); 
+});
+
